b4dcfde7cfa01b30b9723a863d18e7e8f1e6576a
Dean Ayalon <dev@deanayalon.com>
2026-03-03 02:40:19 +0200

    Apply reimplemented secant method into project for test

--- blahblah.java
+++ blahblah.java
@@ -454,76 +454,21 @@ public class MainFrame extends JFrame {
 
     //Secant Method
     private double secantMethod(double a, double b, double epsilon) {
-        //Iteration Counter Reset
-        iterationCount = 0;
-
-        // Start with two slightly different points inside interval
-        double x0 = a;
-        double x1 = a + (b - a) * 0.5;
-
-        // If they are too close, shift slightly
-        if (Math.abs(x1 - x0) < epsilon)
-            x1 = x0 + epsilon * 10;
-
-        double fx0 = evaluateFunction(functionField.getText(), x0);
-        double fx1 = evaluateFunction(functionField.getText(), x1);
-
-        if (Double.isNaN(fx0) || Double.isInfinite(fx0) ||
-                Double.isNaN(fx1) || Double.isInfinite(fx1)) {
-            throw new IllegalArgumentException("Functia trebuie sa fie finita in x0 si x1!");
+        System.out.println("x0 = " + a + ", f(" + a + ") = " + evaluateFunction(functionField.getText(), a));
+        System.out.println("x1 = " + b + ", f(" + b + ") = " + evaluateFunction(functionField.getText(), b));
+        for (iterationCount = 0; iterationCount < 400; iterationCount++) {
+            final double fb = evaluateFunction(functionField.getText(), b);
+            final double fa = evaluateFunction(functionField.getText(), a);
+            final double c = b - fb * (b - a) / (fb - fa);
+            final double fc = evaluateFunction(c);
+            System.out.println("x" + iterationCount + " = " + c + ", f(" + c + ") = " + evaluateFunction(functionField.getText(), c));
+            System.out.println("|f" + iterationCount + "| = " + Math.abs(fc));
+            System.out.println("|x" + iterationCount + " - x" + (n - 1) + "| = " + Math.abs(c - b));
+            if (Math.abs(fc) < epsilon || Math.abs(c - b) < epsilon) return c;
+            a = b;
+            b = c;
         }
-
-        if (Math.abs(fx1 - fx0) < epsilon) {
-            throw new IllegalArgumentException("f(x0) și f(x1) trebuie să fie diferite!");
-        }
-
-        //MAX Iterations for Safety
-        int maxIterations = 1000;
-
-        double xn = x1;
-        double xn_minus_1 = x0;
-        double xn_plus_1;
-
-        //Secant Method Loop
-        while (iterationCount < maxIterations) {
-            iterationCount++;
-
-            /*Calculating next approximation using secant formula
-            xn+1 = xn - f(xn) * (xn - xn-1) / (f(xn) - f(xn-1)) */
-            double fxn = evaluateFunction(functionField.getText(), xn);
-            double fxn_minus_1 = evaluateFunction(functionField.getText(), xn_minus_1);
-
-            if (Double.isNaN(fxn) || Double.isInfinite(fxn) ||
-                    Double.isNaN(fxn_minus_1) || Double.isInfinite(fxn_minus_1))
-                throw new IllegalArgumentException("Secant produced invalid function value");
-
-            //Check if Denominator is NOT 0 (zero)
-            if (Math.abs(fxn - fxn_minus_1) < epsilon * 0.001) {
-                throw new IllegalArgumentException("f(xn) - f(xn-1) este prea mic (diviziune la zero)!");
-            }
-
-            xn_plus_1 = xn - fxn * (xn - xn_minus_1) / (fxn - fxn_minus_1);
-            if (Double.isNaN(xn_plus_1) || Double.isInfinite(xn_plus_1))
-                throw new IllegalArgumentException("Secant produced invalid x");
-
-            double fxn_plus_1 = evaluateFunction(functionField.getText(), xn_plus_1);
-
-            //Check Stopping Criteria
-            if (Math.abs(fxn_plus_1) < epsilon) {
-                return xn_plus_1;
-            }
-
-            if (Math.abs(xn_plus_1 - xn) < epsilon) {
-                return xn_plus_1;
-            }
-
-            //Update for Next Iteration
-            xn_minus_1 = xn;
-            xn = xn_plus_1;
-        }
-
-        //Return Final Approximation
-        return xn;
+        return Double.NaN;
     }
 
     //Newton Method
@@ -675,8 +620,7 @@ public class MainFrame extends JFrame {
 
     private double evaluateFunction(String funcStr, double x) {
 
-        if (Double.isNaN(x) || Double.isInfinite(x))
-            return Double.NaN;
+        if (Double.isNaN(x) || Double.isInfinite(x)) return Double.NaN;
 
         try {
             String expression = funcStr.trim().replace(" ", "");
